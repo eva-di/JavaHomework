679bfb4d904b505c3b661fd973f28884fda3d23e
EvaDi <eva.dieterle.esl@gmail.com>
2025-02-25 12:58:33 +0100

    lesson 21 add

---
+++ src/lesson_21/Dog.java
@@ -0,0 +1,37 @@
+package lesson_21;
+
+public class Dog {
+    String name;
+    int weight;
+
+    public Dog(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+    public void info() {
+        System.out.println("Я собака " + name + ", мой вес : " + weight);
+    }
+
+    public void eat() {
+        System.out.println("Я кушаю, ням-ням!");
+        this.weight++; // Увеличиваем вес на 1
+    }
+
+    public void run() {
+        /*
+        1. Достаточен ли вес собачки для пробежки
+        2.Если вес слишком мал, собака должна поесть перед тренировкой
+         */
+        while (weight < 3) {
+            System.out.println("Сорян, я слишком худая и голодная! Бежать на могу. Надо поесть!");
+            System.out.println("Мой вес сейчас: " + weight);
+            eat();
+        }
+
+        System.out.println("Я бегу!");
+        weight -= 2; //  уменьшаем вес на 2
+        System.out.println("Вес после пробежки:" + weight);
+        System.out.println("=======================\n");
+
+    }
+}
---
+++ src/lesson_21/MainDog.java
@@ -0,0 +1,20 @@
+package lesson_21;
+
+public class MainDog {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Bim", 5);
+        dog.info();
+
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+        dog.run();
+
+
+        dog.info();
+    }
+}
