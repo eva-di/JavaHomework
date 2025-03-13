8265ec4d27c69ab2f66eb09a9ac8b9eefe04bab6
EvaDi <eva.dieterle.esl@gmail.com>
2025-03-13 23:16:18 +0100

    homework 33

---
+++ src/homework_33/Person.java
@@ -0,0 +1,134 @@
+package homework_33;
+
+public class Person {
+    private String email;
+    private String password;
+
+    public Person(String email, String password) {
+        setEmail(email);
+        setPassword(password);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (isEmailValid(email)) {
+            this.email = email;
+        }
+    }
+
+
+    /*
+    test@email.net
+    1. Должна быть 1 @ и только одна
+    2. Должна быть точка после собаки
+    3. после последней точки есть 2 или более символов
+    4. Допустимые символы: алфавит, цифры, '-', '_', '@', '.'
+    5. До собаки должен быть хотя бы 1 символ
+    6. 1 символ должна быть буква
+
+     */
+    private boolean isEmailValid(String email) {
+        // 1. Должна быть 1 @ и только одна
+        int indexAt = email.indexOf('@');
+        int lastAt = email.lastIndexOf('@');
+        if (indexAt == -1 || indexAt != lastAt) return false;
+
+        // 2. Должна быть точка после собаки
+        int dotIndexAfterAt = email.indexOf('.', indexAt + 1);
+        if (dotIndexAfterAt == -1) return false;
+
+        // 3. после последней точки есть 2 или более символов
+        int lastDotIndex = email.lastIndexOf('.');
+        if (lastDotIndex >= email.length() - 2) return false;
+
+        // 4. Допустимые символы: алфавит, цифры, '-', '_', '@', '.'
+        /*
+        Проверяю все символы в строке. Проверяю текущий символ.
+        Если нахожу хоть один неправильный сразу возвращаю false.
+         */
+        // String.toCharArray() - который возвращает массив чаров -> char[]
+
+        for (char ch : email.toCharArray()) {
+
+            // проверка символов на правильность
+            boolean isPass = Character.isAlphabetic(ch)
+                    || Character.isDigit(ch)
+                    || ch == '-'
+                    || ch == '_'
+                    || ch == '.'
+                    || ch == '@';
+            // Если символ не подХодит (isPass = false) возвращаю false
+            if (!isPass) return false; // делай что-то, если переменная false
+        }
+
+        // 5. До собаки должен быть хотя бы 1 символ. То есть индекс собаки не равен 0
+        if (indexAt == 0) return false;
+        // 6. 1 символ должна быть буква. Символ с индексом 0 должна быть буква
+        if (!Character.isLetter(email.charAt(0))) return false;
+
+        // Все проверки пройдены, email подходит
+        return true;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (isPasswordValid(password)) {
+        this.password = password;
+
+        }
+
+    }
+
+
+    /*
+    Требования к паролю:
+    1. Длина пароля больше или равна 8 символам
+    2. Должна быть минимум 1 цифра
+    3. Должна быть мин 1 маленькая буква (Сharacter.isLowerCase();
+    4. Должна быть мин 1 большая буква
+    5. Должен быть мин 1 спец символ: "!№%?*()[],.-"
+
+    Пароль должен удовлетворять всем требованиям
+    5 boolean переменных. Каждая отвечает за свой пункт
+    Пароль будет подходить только если все пять имеют true
+     */
+
+    public boolean isPasswordValid(String password) {
+        if (password.length() < 8) return false;
+
+        boolean hasLower = false;
+        boolean hasUpper = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isDigit(ch)) hasDigit = true;
+            if (Character.isLowerCase(ch)) hasLower = true;
+            if (Character.isUpperCase(ch)) hasUpper = true;
+
+            if( ch == '!' || ch == '@' || ch == '?' || ch == '№'
+            || ch == '%' || ch == '*' || ch == '(' || ch == ')'
+                    || ch == '[' || ch == ']' || ch == ','
+                    || ch == '.' || ch == '-') hasSpecial = true;
+        }
+        if (hasDigit && hasLower && hasUpper && hasSpecial) return true;
+        return false;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Person {" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/homework_33/PersonApp.java
@@ -0,0 +1,24 @@
+package homework_33;
+
+public class PersonApp {
+    public static void main(String[] args) {
+
+//        Person person = new Person("test@mail.net", "password");
+//
+//        person.setEmail("testmail.net");
+//
+//        System.out.println(person);
+//        person.setPassword("passwor");
+//        System.out.println(person);
+
+
+        Person person = new Person("test@mailne.net", "1q@qqwUervbnm");
+
+        person.setEmail("test@ma1.1il.net");
+
+        System.out.println(person);
+
+
+
+    }
+}
