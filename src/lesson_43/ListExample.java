98691c0cb670e8914038784ae0aec695ac16362d
Eva Dieterle <eva.dieterle.esl@gmail.com>
2025-03-27 12:19:17 +0100

    lesson 43

--- src/lesson_43/ListExample.java
+++ src/lesson_43/ListExample.java
@@ -19,5 +19,16 @@ public class ListExample {
         list = new LinkedList<>(); // Создает пустой список (внутренняя реализация - двусвязный список)
         list = new LinkedList<>(List.of(1, -10, 5, 0, 25, -100)); //  Создает список, содержащий все элементы указанной коллекции
 
+        // Методы интерфейса List
+
+        // bolean add (E e) - добавляет элемент в конец списка
+        list.add(1000);
+        list.add(-100);
+        System.out.println("list: " + list);
+
+        // void add(int index, E element) - добавляет элемент на указанную позицию перед index (не переписывает)
+        list.add(3, 500);
+
+        System.out.println("list: " + list);
     }
 }
